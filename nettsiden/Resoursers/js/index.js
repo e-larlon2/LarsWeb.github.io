ebf09263967f0b3a40b00eb7f2bcf2673c7071a6
Lars.github.io <e-larlon2@iskule.no>
2022-02-22 12:52:28 +0100

    m

--- nettsiden/Resoursers/js/index.js
+++ nettsiden/Resoursers/js/index.js
@@ -1,11 +1,13 @@
-// JavaScript Document
 
+// changes the image in OmMeg site to a warning then rick roll image
 function changeImage() {
 	document.getElementById("img").src='../img/rickroll.gif';
-	alert("Warning!!!");
+	alert("Warning!!! i love cumsex");
+	changeImage = function(){}
 }
 	
 function changeBack() {
 	document.getElementById("img").src='../img/mandostranding.jpg';
+	changeBack = function(){}
 }
-	
+
