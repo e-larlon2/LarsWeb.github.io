d5190c7b6be9b444755098de7ab91f66d12fcc77
Lars.github.io <e-larlon2@iskule.no>
2022-02-22 09:44:33 +0100

    m

---
+++ nettsiden/Resoursers/js/index.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+function changeImage() {
+	document.getElementById("img").src='../img/rickroll.gif';
+	alert("Warning!!!");
+}
+	
+function changeBack() {
+	document.getElementById("img").src='../img/mandostranding.jpg';
+}
+	
