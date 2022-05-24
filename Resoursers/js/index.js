745ea88c43d4974d203c0ba708528e481c82ba4e
Lars.github.io <e-larlon2@iskule.no>
2022-05-24 18:02:16 +0200

    nesten

--- Resoursers/js/index.js
+++ Resoursers/js/index.js
@@ -1,11 +1,11 @@
 // changes the image in OmMeg site to a warning then rick roll image
 function changeImage() {
-  document.getElementById("img").src = "../img/rickroll.gif";
+  document.getElementById("img").src = "Resoursers/img/rickroll.gif";
   alert("Warning!!!");
   changeImage = function () {};
 }
 
 function changeBack() {
-  document.getElementById("img").src = "../img/mandostranding.jpg";
+  document.getElementById("img").src = "Resoursers/img/mandostranding.jpg";
   changeBack = function () {};
 }
